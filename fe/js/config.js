c8e1bb18d03fae8eaa0470e9a773ab0b26016a29
VuHuu <162543040+Vucore@users.noreply.github.com>
2025-05-10 14:46:01 +0700

    update firebase, websockets, adaptive rag with embedding

--- fe/js/config.js
+++
@@ -1,4 +0,0 @@
-const API_WS = "ws://localhost:8000" + "/ws"
-const API_CHART = "ws://localhost:8000" + "/ws"
-const API_REGISTER = "//localhost:8000" + "/api/register"
-const API_CHAT = "//localhost:8000" + "/api/chat"
